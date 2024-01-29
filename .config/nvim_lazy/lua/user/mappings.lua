890958957e92a19fa29dd13472a5bb6cae721483
Alexander Kudryavtsev <kai66673@mail.ru>
2024-01-29 18:20:51 +0300

    ...

--- .config/nvim_lazy/lua/user/mappings.lua
+++ .config/nvim_lazy/lua/user/mappings.lua
@@ -5,6 +5,16 @@ vim.keymap.set('n', '<leader>e', ':Neotree left toggle<CR>')
 vim.keymap.set('n', '<leader>o', ':Neotree left focus<CR>')
 vim.keymap.set('n', '<leader><s-e>', ':Neotree float toggle<CR>')
 
--- Tabs
+-- Tabs & buffers...
 vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
 vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+vim.keymap.set('n', '<c-c>', '<CMD>bd<CR>')
+vim.keymap.set('i', '<c-c>', '<CMD>bd<CR>')
+vim.keymap.set('n', '<c-s>', '<CMD>w<CR>')
+vim.keymap.set('i', '<c-s>', '<CMD>w<CR>')
+vim.keymap.set('n', '<c-q>', '<CMD>qa<CR>')
+vim.keymap.set('i', '<c-q>', '<CMD>qa<CR>')
+
+-- LSP-navigation
+vim.keymap.set('n', '<F2>', '<CMD>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('i', '<F2>', '<CMD>lua vim.lsp.buf.definition()<CR>')
